e950b2fde0158d1ce7471fda621aacc4a2b918ff
Alexander Reed <117864716+amr080@users.noreply.github.com>
2024-07-25 19:30:40 -0400

    Add files via upload

---
+++ _next/static/chunks/pages/index-61261dc2e152434f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5405],{87314:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return _(58830)}])},58830:function(n,u,_){"use strict";_.r(u);var t=_(77349);u.default=t.default}},function(n){n.O(0,[7349,9774,2888,179],(function(){return u=87314,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
---
+++ env-config.js
@@ -0,0 +1,13 @@
+window._env_ = {
+  NEXT_TELEMETRY_DISABLED: "1",
+  NEXT_PUBLIC_XSRF_TOKEN_COOKIE_NAME: "__Secure-DBP-CSRF-TOKEN",
+  NEXT_PUBLIC_XSRF_TOKEN_HEADER_NAME: "X-DBP-CSRF-TOKEN",
+  NEXT_PUBLIC_IRIS_AUTH_UI_URL: "https://auth.developer.deutsche-boerse.com",
+  NEXT_PUBLIC_IRIS_EDGE_URL: "https://developer.deutsche-boerse.com",
+  NEXT_PUBLIC_HYDRA_CLIENT_ID: "iris-ui",
+  NEXT_PUBLIC_DISCLAIMER_URL: "https://s3.eu-central-1.amazonaws.com/dbp-prod-public-documents/Userinterface_Disclaimer.pdf",
+  NEXT_PUBLIC_TERMS_AND_CONDITIONS_URL: "https://s3.eu-central-1.amazonaws.com/dbp-prod-public-documents/DBAG_API_Platform_Terms_of_Use.pdf",
+  NEXT_PUBLIC_PRIVACY_POLICY_URL: "https://s3.eu-central-1.amazonaws.com/dbp-prod-public-documents/DBAG_API_Platform_Privacy_Policy.pdf",
+  NEXT_PUBLIC_NEWS_URL: "https://docs.developer.deutsche-boerse.com/blog/2021/06/01/Release-Notes/",
+  NEXT_PUBLIC_PUBLIC_DOCS_URL: "https://docs.developer.deutsche-boerse.com",
+}
